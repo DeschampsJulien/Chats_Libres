aa66221f7768f0292935da4ab9be494ea5bfd986
Julien Deschamps <jdeschamps34@yahoo.fr>
2026-03-29 21:24:50 +0200

    test-bd

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Doctrine\DBAL\Connection;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -15,5 +16,16 @@ final class HomeController extends AbstractController
             'app_name' => 'Chats Libres',
             'database_name' => 'chats_libres',
         ]);
+    }
+
+    #[Route('/test-db')]
+    public function test(Connection $connection): Response
+    {
+        try {
+            $connection->executeQuery('SELECT 1');
+            return new Response('DB OK');
+        } catch (\Exception $e) {
+            return new Response($e->getMessage());
+        }
     }
 }
